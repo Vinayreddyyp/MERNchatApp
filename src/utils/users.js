bb9cd66a227e9f2f93ec172f3f6a8a7dc0e0124d
vinayreddyyp <ypvinayreddy@gmail.com>
2022-06-06 10:40:35 -0700

    add user showing logic to  the window

---
+++ src/utils/users.js
@@ -0,0 +1,25 @@
+const suer = [];
+
+const addUser = ({ id, username, room }) => {
+    username = username.trim().toLowerCase();
+    room = room.trim().toLowerCase();
+    if (!username || !room) return {
+        error: 'Username and room are required'
+    }
+
+    const existingUser = users.find((user) => {
+        return user.room === room && user.username === username;
+
+    })
+
+    if (existingUser) {
+        return {
+            error: 'Username is in use'
+        }
+    }
+
+    const user = { id, username, room };
+    user.push(user)
+    return { user }
+
+}
